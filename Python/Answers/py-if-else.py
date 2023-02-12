fa3c7928b1cff02c044028b72eeb4c06b4a39d46
minus-twelve <121825645+minus-twelve@users.noreply.github.com>
2023-02-12 22:51:35 +0300

    update

---
+++ Python/Answers/py-if-else.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if n%2==1:
+    print('Weird')
+else:
+    if n%2==0 and 2<=n<=5:
+        print('Not Weird')
+    else:
+        if 6<=n<=20:
+            print('Weird')
+        else:
+            if n>20:
+                print('Not Weird')
